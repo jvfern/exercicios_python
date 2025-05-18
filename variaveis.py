41de6c69d98aa350860a86b076d3a28b310ab536
João Vitor Fernandes <90657530+jvfern@users.noreply.github.com>
2025-05-18 23:24:42 +0000

    comentário sobre as variáveis que seriam aplicadas variável nome com o valor João Vitor variável sobrenome com o valor Fernandes variável idade com o valor 21 variável ano nascimento com o valor 2003 variável maior_idade para conferir se é maior de idade variável altura com o valor 1.76 espaço função print com a variável nome para ser vista no terminal função print com a variável sobrenome para ser vista no terminal função print com a variável idade para ser vista no terminal função print com a variável ano_nascimento para ser vista no terminal função print com a variável maior_idade para ser vista no terminal função print com a variável altura para ser vista no terminal

---
+++ variaveis.py
@@ -0,0 +1,14 @@
+# nome, sobrenome, idade, ano de nascimento, é maior de idade?, altura em metros
+nome = 'João Vitor'
+sobrenome = 'Fernandes'
+idade = 21
+ano_nascimento = 2003
+maior_idade = idade >= 18
+altura = 1.76
+
+print('Nome:',nome)
+print('Sobrenome:',sobrenome)
+print('Idade:',idade)
+print('Ano de nascimento:',ano_nascimento)
+print('É maior de idade?:',maior_idade)
+print('Altura em metros:',altura, end=' metros')
